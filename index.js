75bd9896bc0a607de6b64e3a7ccdda396336a4ba
kda08 <kda082@gmail.com>
2020-12-06 20:43:42 +0900

    키 보호

--- index.js
+++ index.js
@@ -2,7 +2,23 @@ const express = require('express')
 const app = express()
 const port = 5000  
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://test:1234@sc.lakw3.mongodb.net/sc?retryWrites=true&w=majority',{
+
+
+const bodyParser = require('body-parser');
+const { User } = require('./models/User.js');
+
+
+const config = require('./config/key');
+
+
+//application/x-www-form-urlencoded 데이터를 분석할수 있게
+app.use(bodyParser.urlencoded({extended:true}));
+
+// application/json으로 가져오는 데이터를 분석할수 있게
+app.use(bodyParser.json());
+
+
+mongoose.connect(config.mongoURI,{
     useNewUrlParser: true
     ,useUnifiedTopology:true
     ,useCreateIndex:true
@@ -12,7 +28,29 @@ mongoose.connect('mongodb+srv://test:1234@sc.lakw3.mongodb.net/sc?retryWrites=tr
 
 
 app.get('/',(req,res) => res.send('안녕하세요'))  // 루트 디렉토리에 출력
+
+
+// 회원 가입을 위한 route 만들다
+
+app.post('/register',(req,res) => {
+
+// 회원 가입 할때 필요한 정보들을 client에서 가져오면 
+// 그것들을 데이터 베이스에 넣어준다.
+    const user = new User(req.body)
+
+    
+    user.save((err,userInfo)  =>  {
+        if(err) return res.json({success:false,err})
+        return res.status(200).json({
+            success:true
+        })
+     }) //mongodb의 함수 유저모델을 저장해준다.
+})
+
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
+
 
 
 
--- models/User.js
+++ models/User.js
@@ -1,1 +1,38 @@
 const mongoose = require('mongoose');
+
+
+//스키마 생성
+const userSchema = mongoose.Schema({
+    nmae : {
+        type: String,
+        maxlength: 50
+    },
+    email:{
+        type: String,
+        trim: true, // 공백제거
+    },
+    lastname:{
+        type: String,
+        maxlength: 50
+    },
+    role:{
+        type: Number,
+        default: 0
+    },
+    image:String,
+    token: {
+        type:String
+    },
+    tokenExp:{  //토큰 유효기간
+        type:Number
+    }
+})
+
+// 스키마를 모델로 감싸준다.
+const User = mongoose.model('User',userSchema)  ;  //모델이름,스키마
+
+// 모델을 외부파일에서도 쓸 수 있게 module.exports
+
+module.exports = {
+    User
+};
